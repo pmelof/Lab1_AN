73068b2319ae5decb428c32cd3336e3ee3ffea50
Patricia Melo <patricia.melo@usach.cl>
2019-05-03 15:24:06 -0400

    LU y RQ

---
+++ funcion_LU.m
@@ -0,0 +1,16 @@
+%Función que descompone la matriz A en dos matrices: U (mtriz triangular superior) y 
+%L (matriz triangular inferior), donde A = L*U.
+
+function [L,U]=funcion_LU(A)
+
+n = length(A(:,1));          %n° de filas.
+L=A*0;                       %matrices L y U inicializadas en 0.
+U=A*0;
+
+for k=1:n
+    U(k,k:n) = A(k,k:n)-L(k,1:k-1)*U(1:k-1,k:n);
+    L(k,k) = 1;
+    L(k+1:n,k) = (A(k+1:n,k)-L(k+1:n,1:k-1)*U(1:k-1,k))/U(k,k);
+
+end
+end
---
+++ funcion_gs.m
@@ -0,0 +1,19 @@
+%Función que descompone la matriz A en dos matrices: Q y R, 
+%donde A = Q*R.
+%Aunque parece que en este caso A=R*Q.
+
+function [Q,R] = funcion_gs(A)
+
+[m, n] = size(A);
+Q = A;
+R = zeros(n);
+
+for k=1:n
+    for i=1:k-1
+        R(i, k) = Q(:, i)' * A(:, k);
+        Q(:, k) = Q(:, k) - Q(:, i) * R(i, k);
+    end
+    R(k, k) = norm(Q(:, k));
+    Q(:, k) = Q(:, k)/R(k, k);
+end
+end
